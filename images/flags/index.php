29690fc293da06f86eb4e279d49568d058fe4f6a
Pat MacMannis <pat@macmannis.com>
2017-01-19 23:37:07 -0500

    Initial Commit - /images pt 2

---
+++ images/flags/index.php
@@ -0,0 +1,23 @@
+<?php
+/** sub-dir index.php **/
+
+echo "<META HTTP-EQUIV='Refresh' Content='0; URL=./game/login.php'>";
+
+include ('./game/header.php');
+?>
+<table align='center' border='0'>
+	<tr valign='top'>
+		<td>
+			<tr>
+				<td>
+					<table align='center' valign='middle'>
+						<h1>ACCESS DENIED!</h1>
+					</table>
+				</td>
+			</tr>
+		</td>
+	</tr>
+</table>
+<?php
+include ('./game/footer.php');
+?>
